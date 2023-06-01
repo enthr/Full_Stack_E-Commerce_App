40c789e88d7e480839b73bc14cef1ef4bb547b8d
Jaimin Prajapati <enthralall@gmail.com>
2023-06-01 23:22:10 +0530

    Auth Controllers Defined

---
+++ ecomm_backend/controllers/auth.controller.js
@@ -0,0 +1,213 @@
+import User from '../models/user.schema.js';
+import Token from '../models/token.schema.js';
+import asyncHandler from '../utils/asyncHandler.js';
+import CustomError from '../utils/customError.js';
+import mailHelper from '../utils/mailHelper.js';
+
+export const cookieOptions = {
+    expires: new Date(Date.now() + 3 * 24 * 60 * 60 * 1000),
+    httpOnly: true,
+};
+
+/******************************************************
+ * @SIGNUP
+ * @route http://localhost:5000/api/auth/signup
+ * @description User signUp Controller for creating new user
+ * @returns User Object
+ ******************************************************/
+
+export const signUp = asyncHandler(async (req, res) => {
+    const { firstName, lastName, role, contact, email, password } = req.body;
+
+    if (!firstName || !lastName || !email || !password || !role || !contact) {
+        throw new CustomError("Please fill all the fields", 400);
+    }
+
+    const existingUser = await User.findOne({ email });
+
+    if (existingUser) {
+        throw new CustomError("User already exists", 400);
+    }
+
+    const user = await User.create({
+        firstName,
+        lastName,
+        role,
+        contact,
+        email,
+        password
+    });
+
+    user.save();
+    const token = user.getJwtToken();
+    user.password = undefined;
+
+    res.cookie("token", token, cookieOptions);
+
+    res.status(200).json({
+        success: true,
+        token,
+        user
+    });
+});
+
+/*********************************************************
+ * @LOGIN
+ * @route http://localhost:5000/api/auth/login
+ * @description User Login Controller for signing in the user
+ * @returns User Object
+ *********************************************************/
+export const login = asyncHandler(async (req, res) => {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+        throw new CustomError("Please fill all the fields", 400);
+    }
+
+    const user = await User.findOne({ email });
+
+    if (!user) {
+        throw new CustomError("Invalid credentials", 400);
+    }
+
+    const isPasswordMatched = await user.comparePassword(password);
+    if (isPasswordMatched) {
+        if (user.status === "BLOCKED") {
+            throw new CustomError("Your account has been blocked, Please Contact Admin", 400);
+        }
+
+        if (user.status === "UNVERIFIED") {
+            throw new CustomError("Please verify your email", 400);
+        }
+
+        const token = user.getJwtToken();
+        user.password = undefined;
+        res.cookie("token", token, cookieOptions);
+        return res.status(200).json({
+            success: true,
+            token,
+            user
+        });
+    } else {
+        throw new CustomError("Password is incorrect", 400);
+    }
+});
+
+/**********************************************************
+ * @LOGOUT
+ * @route http://localhost:5000/api/auth/logout
+ * @description User Logout Controller for logging out the user
+ * @description Removes token from cookies
+ * @returns Success Message with "Logged Out"
+ **********************************************************/
+export const logout = asyncHandler(async (_req, res) => {
+    res.cookie("token", null, {
+        expires: new Date(Date.now()),
+        httpOnly: true
+    });
+
+    res.status(200).json({
+        success: true,
+        message: "Logged out",
+    });
+});
+
+/**********************************************************
+ * @FORGOT_PASSWORD
+ * @route http://localhost:5000/api/auth/password/forgot
+ * @description ForgotPassword Controller for genrating forgotPassword Token
+ * @description Sends Reset password link through email using [nodmailer, mailtrap]
+ * @returns Success Message with "Email Sent"
+ **********************************************************/
+export const forgotPassword = asyncHandler(async (req, res) => {
+    const { email } = req.body;
+    const user = await User.findOne({ email });
+
+    if (!user) {
+        throw new CustomError("User not found", 404);
+    }
+
+    const token = await Token.create({ user: user._id });
+    const resetToken = token.generateToken();
+    await token.save({ validateBeforeSave: false });
+
+    const resetUrl = `${req.protocol}://${req.get("host")}/api/auth/password/reset/${resetToken}`;
+
+    const message = `Your password reset token is as follow:\n\n${resetUrl}\n\nIf you have not requested this email, then ignore it.`;
+
+    try {
+        await mailHelper({
+            email: user.email,
+            subject: "Password reset token",
+            message
+        });
+
+        res.status(200).json({
+            success: true,
+            message: `Email sent to: ${user.email}`,
+        });
+    } catch (err) {
+        token.status = "EXPIRED";
+        token.expiresAt = Date.now();
+        await token.save({ validateBeforeSave: false });
+
+        throw new CustomError(err.message || "Email could not be sent", 500);
+    }
+});
+
+/**********************************************************
+ * @RESET_PASSWORD
+ * @route http://localhost:5000/api/auth/password/reset/:resetPasswordToken
+ * @description resetPassword Controller for reseting password of the user
+ * @returns User Object with new password
+ **********************************************************/
+export const resetPassword = asyncHandler(async (req, res) => {
+    const { token: resetToken } = req.params;
+    const { password, confirmPassword, email } = req.body;
+
+    if (password !== confirmPassword) {
+        throw new CustomError("Password does not match", 400);
+    }
+
+    const user = User.findOne({ email });
+    const storedToken = Token.findOne({ user: user._id });
+    const isTokenValid = await storedToken.verifyToken(resetToken);
+
+    if (!isTokenValid) {
+        throw new CustomError("Password reset token is invalid or has expired", 400);
+    }
+
+    user.password = password;
+
+    await user.save();
+
+    const token = user.getJwtToken();
+    user.password = undefined;
+
+    res.cookie("token", token, cookieOptions);
+
+    res.status(200).json({
+        success: true,
+        user
+    });
+});
+
+/**********************************************************
+ * @GET_PROFILE
+ * @route http://localhost:5000/api/auth/profile
+ * @description check token in cookies, if present then returns user details
+ * @returns Logged In User Details
+ **********************************************************/
+
+export const getProfile = asyncHandler(async (req, res) => {
+    const user = await User.findById(req.user._id);
+
+    if (!user) {
+        throw new CustomError("User not found", 404);
+    }
+
+    res.status(200).json({
+        success: true,
+        user
+    });
+});
